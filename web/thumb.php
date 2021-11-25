f8170b469d828208fcf928e9ec037d88060c983d
Petarr <pierre.lariviere@intercode.ca>
2021-11-24 23:27:35 -0500

    bigger thumb for new design

--- web/thumb.php
+++ web/thumb.php
@@ -4,4 +4,4 @@ if(!isset($_GET['video']) || !isset($_GET['chunk']))
 
 include(dirname(dirname(__FILE__)).'/include/init.php');
 include(BP.'/include/thumbalizer.php');
-get_image($_GET['video'],$_GET['chunk'],200,200);
+get_image($_GET['video'],$_GET['chunk'],390,219);
